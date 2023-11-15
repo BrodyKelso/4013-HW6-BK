b9a2be0f2bb597f491e760585f8c6f181b2dde0d
BrodyKelso <Brody.P.Kelso-1@ou.edu>
2023-11-14 20:47:26 -0600

    done.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     // Sample quiz data
     const questions = [
-        { question: "What is 2+2?", choices: ["3", "4", "5", "6"], answer: "4" },
-        { question: "What is the capital of France?", choices: ["Paris", "London", "Berlin", "Rome"], answer: "Paris" }
+        { question: "Have you ever had a DP shootout?", choices: ["Yes", "No",], answer: "Yes" },
+        { question: "When was the first HTML dirty joke posted on the internet?", choices: ["1989, with the advent of the World Wide Web.", "2001, marking a humorous milestone in the early days of social media platforms.", "1995, coinciding with the widespread popularity of early internet forums.", "1998, during the boom of personal websites and user-generated content."], answer: "1998, during the boom of personal websites and user-generated content." }
         // Add more questions as needed
     ];
 
     let currentQuestionIndex = 0;
     let score = 0;
 
-    // Display the current date using Moment.js
+    
     $('#current-date').text(moment().format('MMMM Do YYYY'));
 
-    // Display the first question
+    
     displayQuestion();
 
-    // On submitting an answer
+    
     $('#submit-btn').click(function() {
         const selectedChoice = $('input[name="choice"]:checked').val();
         checkAnswer(selectedChoice);
